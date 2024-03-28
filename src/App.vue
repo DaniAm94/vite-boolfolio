6fc38f44d24544c092e086a9ad6ec34026d95a24
DaniAm94 <dani.amico94@gmail.com>
2024-03-28 17:15:16 +0100

    updated a function to fetch projects at app creation and showed them into the page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script>
-//Javascript
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const defaulEndpoint = 'http://localhost:8000/api/projects/';
 export default {
-  name: 'Boolfolio'
+  name: 'Boolfolio',
+  components: { AppHeader },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    fetchProjects(endpoint) {
+      axios.get(endpoint).then(res => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects(defaulEndpoint);
+  }
 };
 </script>
 
 <template>
+  <AppHeader />
   <main class="container py-3">
-    <header>
-      <h1>Boolfolio</h1>
-    </header>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h3>{{ project.title }}</h3>
+
+      </li>
+    </ul>
   </main>
 </template>
 
