f147abcdc14dcf934b32382b82591e42ddbf1d9a
DaniAm94 <dani.amico94@gmail.com>
2024-03-28 16:55:03 +0100

    first commit for frontend boolfolio project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+//Javascript
+export default {
+  name: 'Boolfolio'
+};
+</script>
+
+<template>
+  <main class="container py-3">
+    <header>
+      <h1>Boolfolio</h1>
+    </header>
+  </main>
+</template>
+
+<style>
+/*Css*/
+</style>
