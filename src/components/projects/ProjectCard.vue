5bf7d9281f4197c5839b6546be08d458882ee6f3
DaniAm94 <dani.amico94@gmail.com>
2024-03-28 18:02:21 +0100

    fixed minutes format

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -24,9 +24,10 @@ export default {
             let month = date.getMonth() + 1;
             const year = date.getFullYear();
             const hours = date.getHours();
-            const minutes = date.getMinutes();
+            let minutes = date.getMinutes();
             const seconds = date.getSeconds();
 
+            if (minutes < 10) minutes = '0' + minutes;
             if (day < 10) day = '0' + day;
             if (month < 10) month = '0' + month;
             return `${day}/${month}/${year} alle ${hours}:${minutes}`;
