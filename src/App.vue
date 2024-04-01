480eb9f3604771787bb777d46ce8ea426825dcb5
DaniAm94 <dani.amico94@gmail.com>
2024-04-01 17:50:06 +0200

    update app component to host new components

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,39 @@
 import AppHeader from './components/AppHeader.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
 import ProjectCard from './components/projects/ProjectCard.vue';
+import AppAlert from './components/AppAlert.vue';
+import BasePagination from './components/BasePagination.vue';
 import axios from 'axios';
-const defaulEndpoint = 'http://localhost:8000/api/projects/';
+import { reactive } from 'vue';
+const defaultEndpoint = 'http://localhost:8000/api/projects/';
 export default {
   name: 'Boolfolio',
-  components: { AppHeader, ProjectCard, ProjectsList },
+  components: { AppHeader, ProjectCard, ProjectsList, AppAlert, BasePagination },
   data: () => ({
-    projects: []
+    projects: {
+      data: [],
+      links: []
+    },
+    isLoading: false,
+    isAlertOpen: false
   }),
   methods: {
-    fetchProjects(endpoint) {
+    fetchProjects(endpoint = defaultEndpoint) {
+      this.isLoading = true;
       axios.get(endpoint).then(res => {
-        this.projects = res.data;
+        this.isAlertOpen = false;
+        const { data, links } = res.data;
+        this.projects = { data, links };
+      }).catch(err => {
+        console.error(err);
+        this.isAlertOpen = true;
+      }).then(() => {
+        this.isLoading = false;
       })
     }
   },
   created() {
-    this.fetchProjects(defaulEndpoint);
+    this.fetchProjects(defaultEndpoint);
   }
 };
 </script>
@@ -26,7 +42,13 @@ export default {
 <template>
   <AppHeader />
   <main class="container pt-5">
-    <ProjectsList :projects="projects" />
+    <h1>Boolfolio</h1>
+    <AppAlert :show="isAlertOpen" @close="isAlertOpen = false" @retry="fetchProjects" />
+    <AppLoader v-if="isLoading" />
+    <div v-else>
+      <ProjectsList :projects="projects.data" />
+      <BasePagination :links="projects.links" @changePage="fetchProjects" />
+    </div>
   </main>
 </template>
 
