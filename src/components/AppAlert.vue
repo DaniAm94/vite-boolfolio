0f3d5152552b0d5b402f344086aaeb7ead4410cd
DaniAm94 <dani.amico94@gmail.com>
2024-04-01 17:48:17 +0200

    created an AppAlert component

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'App Alert',
+    props: {
+        show: Boolean
+    },
+    emits: ['close', 'retry']
+}
+</script>
+
+<template>
+    <div v-if="show" class="alert alert-danger alert-dismissible fade show" role="alert">
+        Si è verificato un errore. <strong class="text-decoration-underline" role="button"
+            @click="$emit('retry')">Riprova</strong>
+        <button type="button" class="btn-close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<style>
+/*Css*/
+</style>
