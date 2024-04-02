9793784fa843402e8eb9194b76bb319b919efdb5
DaniAm94 <dani.amico94@gmail.com>
2024-04-02 23:48:06 +0200

    added a new route for the detail page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // Importo le pagine
 import HomePage from '../pages/HomePage.vue';
+import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
 import ContactUsPage from '../pages/ContactUsPage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
 
@@ -12,6 +13,7 @@ const router = createRouter({
     linkExactActiveClass: 'active',
     routes: [
         { path: '/', component: HomePage, name: 'home' },
+        { path: '/projects/:slug', component: ProjectDetailPage, name: 'project-detail' },
         { path: '/contact-us', component: ContactUsPage, name: 'contact-us' },
         { path: '/not-found', component: NotFoundPage, name: 'not-found' },
         { path: '/:pathMatch(.*)*', redirect: '/not-found' }
