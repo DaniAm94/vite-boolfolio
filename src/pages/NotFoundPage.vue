b3356183042d9e993e785d838a721492e73405ca
DaniAm94 <dani.amico94@gmail.com>
2024-04-02 15:52:37 +0200

    added a go back button into the not found page

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -5,8 +5,10 @@ export default {
 </script>
 
 <template>
-    <div class="d-flex justify-content-center align-items-center" style="height: calc(100vh - 104px);">
+    <div class="d-flex flex-column row-gap-4 justify-content-center align-items-center"
+        style="height: calc(100vh - 104px);">
         <h1>404 | Not Found</h1>
+        <button class="btn btn-secondary " @click="$router.back()">Torna Indietro</button>
     </div>
 </template>
 
