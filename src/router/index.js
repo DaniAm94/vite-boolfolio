2870ea0bea0c1ce315316d47c221bb634734bedc
DaniAm94 <dani.amico94@gmail.com>
2024-04-02 15:36:31 +0200

    created a route for the not found page and a redirect to it for wrong urls

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 // Importo le pagine
 import HomePage from '../pages/HomePage.vue';
 import ContactUsPage from '../pages/ContactUsPage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 // Definisco le rotte
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage },
-        { path: '/contact-us', component: ContactUsPage }
+        { path: '/contact-us', component: ContactUsPage },
+        { path: '/not-found', component: NotFoundPage },
+        { path: '/:pathMatch(.*)*', redirect: '/not-found' }
     ]
 });
 
