db4afa4675792c0f0ee72406afa8df8bbc5c92e5
DaniAm94 <dani.amico94@gmail.com>
2024-04-02 15:44:54 +0200

    handled active class on navbar's links

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import NotFoundPage from '../pages/NotFoundPage.vue';
 // Definisco le rotte
 const router = createRouter({
     history: createWebHistory(),
+    linkActiveClass: 'active',
+    linkExactActiveClass: 'active',
     routes: [
         { path: '/', component: HomePage, name: 'home' },
         { path: '/contact-us', component: ContactUsPage, name: 'contact-us' },
