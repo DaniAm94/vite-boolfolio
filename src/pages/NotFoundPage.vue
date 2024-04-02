ba783b05ec46f1b55d69994d5e9ffee7fd957c2c
DaniAm94 <dani.amico94@gmail.com>
2024-04-02 15:35:37 +0200

    created a not found page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'Not Found Page'
+}
+</script>
+
+<template>
+    <div class="d-flex justify-content-center align-items-center" style="height: calc(100vh - 104px);">
+        <h1>404 | Not Found</h1>
+    </div>
+</template>
+
+<style>
+/*Css*/
+</style>
