7fc17f3b85206eb305bcc3f7553dc9452bdb95f4
DaniAm94 <dani.amico94@gmail.com>
2024-04-03 16:15:03 +0200

    created a new route to the type projects page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import HomePage from '../pages/HomePage.vue';
 import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
 import ContactUsPage from '../pages/ContactUsPage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import TypeProjectsPage from '../pages/TypeProjectsPage.vue';
 
 // Definisco le rotte
 const router = createRouter({
@@ -14,6 +15,7 @@ const router = createRouter({
     routes: [
         { path: '/', component: HomePage, name: 'home' },
         { path: '/projects/:slug', component: ProjectDetailPage, name: 'project-detail' },
+        { path: '/type/:slug/projects', component: TypeProjectsPage, name: 'type-projects' },
         { path: '/contact-us', component: ContactUsPage, name: 'contact-us' },
         { path: '/not-found', component: NotFoundPage, name: 'not-found' },
         { path: '/:pathMatch(.*)*', redirect: '/not-found' }
